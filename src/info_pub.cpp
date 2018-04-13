a47b2a8ec780928b0333c407fde5230f65d817cb
PierBanda <37575086+PierBanda@users.noreply.github.com>
2018-04-13 23:21:16 +0200

    Update info_pub.cpp

--- src/info_pub.cpp
+++ src/info_pub.cpp
@@ -1,57 +1,39 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include <homework1/info_msg.h>
-#include "my_database.h"
+#include "ros/ros.h" /*ros library*/
+#include <homework1/info_msg.h> /*custom message type*/
+#include "my_database.h" /*header file whith informations abaut people*/
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 
-/**
-* This tutorial demonstrates simple receipt of messages over the ROS system.
-*/
-/**void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
-**/
-
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "Info_pub");
+	ros::init(argc, argv, "Info_pub"); /*inizialize ros node*/
 	ros::NodeHandle n;
 	
-	ros::Publisher chatter_pub = n.advertise<homework1::info_msg>("info_persona", 1000);
+	ros::Publisher chatter_pub = n.advertise<homework1::info_msg>("info_persona", 1000); /*inizialize topic*/
 
-  	ros::Rate loop_rate(1);
+  	ros::Rate loop_rate(1); /*1 Hz*/
 	
-	srand (time(NULL));
+	srand (time(NULL)); /*set random seed*/
 	
-	homework1::info_msg persona;
+	homework1::info_msg persona; /*create a message object */ 
 
-	while (ros::ok())
-		{
-		/**
-		* This is a message object. You stuff it with data, and then publish it.
-		*/
-		
-		persona.name=names[rand() % 5];
-		persona.age=rand() % 20 +18;
-		persona.corso=corsi[rand() % 5];
-		
-		/**std::stringstream ss;
-		ss << "hello world " << count;
-		homework1.data = ss.str();**/
-
+	while (ros::ok()) 
+		{ /*setting the info that will be pubblished*/
+		persona.name=names[rand() % 5]; /*take a rand name from my_database.h*/
+		persona.age=rand() % 20 +18; /*take a rand number between 18-38*/
+		persona.corso=corsi[rand() % 5]; /*take a rand corso from my_database.h*/
+		/* show the current data of persona
 		ROS_INFO("%s eta %i del corso di %s",persona.name.c_str() ,persona.age, persona.corso.c_str());
-
+		*/
 		/**
 		* The publish() function is how you send messages. The parameter
 		* is the message object. The type of this object must agree with the type
 		* given as a template parameter to the advertise<>() call, as was done
 		* in the constructor above.
 		*/
-		chatter_pub.publish(persona);
+		chatter_pub.publish(persona); /*publishing the data*/
 
-		ros::spinOnce();
+		ros::spinOnce(); 
 
 		loop_rate.sleep();
 		}
